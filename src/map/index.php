6974bc9ad1aa01816840a5c90c4c4a2219a89bb0
wgetJane <margjane@keemail.me>
2025-09-25 21:51:47 +0800

    add map and mod redirects

---
+++ src/map/index.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<meta http-equiv="refresh" content="0; url='/maps/newest/'"/>
+		<meta name="robots" content="noindex" />
+		<link rel="stylesheet" href="/gutworx/stylesheets/cruscc.css">
+		<link rel="icon" href="/gutworx/images/cruelty_squad/cruscc.ico">
+		<script>
+			window.location.href = "/maps/newest/";
+		</script>
+	</head>
+	<title>REDIRECTING - CRUELTY SQUAD CUSTOM CONTENT</title>
+	<body id="terminal">
+		<b>Redirecting... balls</b>
+	</body>
+</html>
---
+++ src/mod/index.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<meta http-equiv="refresh" content="0; url='/mods/newest/'"/>
+		<meta name="robots" content="noindex" />
+		<link rel="stylesheet" href="/gutworx/stylesheets/cruscc.css">
+		<link rel="icon" href="/gutworx/images/cruelty_squad/cruscc.ico">
+		<script>
+			window.location.href = "/mods/newest/";
+		</script>
+	</head>
+	<title>REDIRECTING - CRUELTY SQUAD CUSTOM CONTENT</title>
+	<body id="terminal">
+		<b>Redirecting... balls</b>
+	</body>
+</html>

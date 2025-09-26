03c31129eb0353b7db132d8098d4ffdac843f81e
wgetJane <margjane@keemail.me>
2025-09-26 20:00:04 +0800

    set up a bunch of core functions

--- src/gutworx/core.php
+++ src/gutworx/core.php
@@ -1,34 +1,34 @@
 <?php
 
-$_MAP_VARS = [
-	"MAP_NAME",
-	"MAP_AUTHOR",
-	"MAP_VERSION",
-	"MAP_DATE",
-	"MAP_RANK",
-];
-
-$_MOD_VARS = [
-	"MOD_NAME",
-	"MOD_AUTHOR",
-	"MOD_VERSION",
-	"MOD_DATE",
-	"MOD_RANK",
-];
-
-function component(string $path) {
+function component(string $path): void {
 	global $ROOT;
 	include($ROOT . "gutworx/components/" . $path . ".php");
 }
 
-function getMaps() {
+function getCallerScript(): string {
+	return debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 2)[1]["file"];
+}
+
+function getCallerBaseDir(): string {
+	return basename(dirname(getCallerScript()));
+}
+
+function getMaps(): array {
 	global $_MAPS;
 
 	if (isset($_MAPS)) {
 		return $_MAPS;
 	}
 
-	global $ROOT, $_MAP_VARS;
+	global $ROOT;
+
+	$var_names = [
+		"MAP_NAME" => true,
+		"MAP_AUTHOR" => true,
+		"MAP_VERSION" => true,
+		"MAP_DATE" => true,
+		"MAP_RANK" => false,
+	];
 
 	$_MAPS = [];
 
@@ -43,32 +43,48 @@ function getMaps() {
 			continue;
 		}
 
-		foreach ($_MAP_VARS as $var_name) {
-			unset($$var_name);
+		foreach ($var_names as $key => $required) {
+			unset($$key);
 		}
 
 		require $info_path;
 
-		foreach ($_MAP_VARS as $var_name) {
-			if (isset($$var_name)) {
-				$info[$var_name] = $$var_name;
+		$info = [];
+
+		foreach ($var_names as $key => $required) {
+			if (isset($$key)) {
+				$info[$key] = $$key;
+			} elseif ($required) {
+				fwrite(STDERR, "Warning: \${$key} is not set in map/{$dir}/info.php" . PHP_EOL);
 			}
 		}
+
+		$info["DateTimeObj"] = DateTime::createFromFormat("j/M/Y", $info["MAP_DATE"]);
 
 		$_MAPS[$dir->getFilename()] = $info;
 	}
 
+	uasort($_MAPS, fn($a, $b) => $b["DateTimeObj"] <=> $a["DateTimeObj"]);
+
 	return $_MAPS;
 }
 
-function getMods() {
+function getMods(): array {
 	global $_MODS;
 
 	if (isset($_MODS)) {
 		return $_MODS;
 	}
 
-	global $ROOT, $_MOD_VARS;
+	global $ROOT;
+
+	$var_names = [
+		"MOD_NAME" => true,
+		"MOD_AUTHOR" => true,
+		"MOD_VERSION" => true,
+		"MOD_DATE" => true,
+		"MOD_RANK" => false,
+	];
 
 	$_MODS = [];
 
@@ -83,28 +99,68 @@ function getMods() {
 			continue;
 		}
 
-		foreach ($_MOD_VARS as $var_name) {
-			unset($$var_name);
+		foreach ($var_names as $key => $required) {
+			unset($$key);
 		}
 
 		require $info_path;
 
-		foreach ($_MOD_VARS as $var_name) {
-			if (isset($$var_name)) {
-				$info[$var_name] = $$var_name;
+		$info = [];
+
+		foreach ($var_names as $key => $required) {
+			if (isset($$key)) {
+				$info[$key] = $$key;
+			} elseif ($required) {
+				fwrite(STDERR, "Warning: \${$key} is not set in mod/{$dir}/info.php" . PHP_EOL);
 			}
 		}
+
+		$info["DateTimeObj"] = DateTime::createFromFormat("j/M/Y", $info["MOD_DATE"]);
 
 		$_MODS[$dir->getFilename()] = $info;
 	}
 
+	uasort($_MODS, fn($a, $b) => $b["DateTimeObj"] <=> $a["DateTimeObj"]);
+
 	return $_MODS;
 }
 
-function getMapCount() {
+function getMapCount(): int {
 	return count(getMaps());
 }
 
-function getModCount() {
+function getModCount(): int {
 	return count(getMods());
+}
+
+function getMapName(string $map_id = ""): string {
+	if ($map_id) {
+		$map_id = getCallerBaseDir();
+	}
+
+	return getMaps()[$map_id]["MAP_NAME"];
+}
+
+function getMapAuthorId(string $map_id = ""): string|array {
+	if ($map_id) {
+		$map_id = getCallerBaseDir();
+	}
+
+	return getMaps()[$map_id]["MAP_AUTHOR"];
+}
+
+function getMapAuthorName(string $map_id = ""): string|array {
+	$author_id = getMapAuthorId($map_id);
+
+	return is_array($author_id)
+		? array_map("getUserName", $author_id)
+		: getUserName($author_id);
+}
+
+function getUserName(string $user_id = ""): string {
+	if ($user_id) {
+		$user_id = getCallerBaseDir();
+	}
+
+	return $user_id;
 }
---
+++ src/map/sin_space_extended/info.php
@@ -0,0 +1,9 @@
+<?php
+$MAP_NAME = "Sin Space Extended";
+$MAP_AUTHOR = "uggo";
+$MAP_VERSION = "test";
+$MAP_DATE = "13/APR/2021";
+
+$MAP_DOWNLOAD = [
+	"https://www.mediafire.com/file/o7t9u6wbcv64d00/Sin_Space_Extended.zip/file",
+];

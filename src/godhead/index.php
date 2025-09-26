8ff8792e7b3c76d6205448d47f31b4e80f2e5222
wgetJane <margjane@keemail.me>
2025-09-26 20:28:53 +0800

    add missing redirects

--- src/godhead/index.php
+++ src/godhead/index.php
@@ -1,11 +1,11 @@
 <html>
 	<head>
-		<meta http-equiv="refresh" content="0; url='/maps/newest/'"/>
+		<meta http-equiv="refresh" content="0; url='/godhead_heavy_industries/'"/>
 		<meta name="robots" content="noindex" />
 		<link rel="stylesheet" href="/gutworx/stylesheets/cruscc.css">
 		<link rel="icon" href="/gutworx/images/cruelty_squad/cruscc.ico">
 		<script>
-			window.location.href = "/maps/newest/";
+			window.location.href = "/godhead_heavy_industries/";
 		</script>
 	</head>
 	<title>REDIRECTING - CRUELTY SQUAD CUSTOM CONTENT</title>

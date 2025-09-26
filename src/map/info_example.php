fed982d4796ec36e9d8d72532a33fbf6a03c1a61
wgetJane <margjane@keemail.me>
2025-09-26 18:35:21 +0800

    add example info.php format

---
+++ src/map/info_example.php
@@ -0,0 +1,69 @@
+<?php
+
+// full name of the map
+$MAP_NAME = "Example Map Name";
+
+// author's id, not full name, example: use "keith_mason" instead of "Keith Mason"
+$MAP_AUTHOR = "example_author";
+// use a list for $MAP_AUTHOR if there's multiple authors
+$MAP_AUTHOR = [
+	"example_author1", "example_author2", "example_author3",
+];
+
+// latest version string, can be anything, doesn't have to be numbers
+$MAP_VERSION = "1.0.0";
+
+// submission date, needs to be in DD/MMM/YYYY format
+// month part should be exactly three letters long
+$MAP_DATE = "01/JAN/1970";
+
+// optional, overrides the calculated values
+// possible values are: New, S, A, B, C, X
+$MAP_RANK = "New";
+
+// list of download links for the map
+$MAP_DOWNLOAD = [
+	// first link is the main download
+	// name and hover for entries like these are automagically parsed
+	"https://www.mediafire.com/file/example_map.zip",
+	"https://www.dropbox.com/scl/fi/example_map.zip",
+	// for more control, use an associative array like this:
+	[
+		// optional, automatically parsed from link
+		"name" => "Google Drive",
+		// required
+		"link" => "https://drive.google.com/file/d/EXAMPLEMAP/view",
+		// optional, defaults to "Download from {name}"
+		"hover" => "Download from Google Drive",
+	],
+	[
+		"name" => "2",
+		"link" => "https://drive.google.com/file/d/EXAMPLEMAPAGAIN/view",
+		"hover" => "Download from Google Drive (Mirror)",
+	],
+];
+
+// optional
+$MAP_ARCHIVE = [
+	[
+		// optional, defaults to current map name
+		"name" => "Example Beta Map",
+		// required, same rules as $MAP_VERSION
+		"version" => "0.1.0",
+		// required, same rules as $MAP_DOWNLOAD
+		"download" => [
+			"https://www.mediafire.com/file/example_map_beta.zip",
+		],
+	],
+	[
+		"name" => "Example Alpha Map",
+		"version" => "0.0.1",
+		"download" => [
+			"https://www.mediafire.com/file/example_map_alpha.zip",
+		],
+	],
+];
+
+// you can also make any custom variable
+// you may use this for map description and objectives or whatever if you want
+$MAP_EXAMPLEVAR = "example value";

42ee35cf4c61a4dcc867bcd2581cefaeed8911cb
wgetJane <margjane@keemail.me>
2025-09-27 18:04:22 +0800

    prefix with ? for non-godhead author names

--- src/gutworx/core.php
+++ src/gutworx/core.php
@@ -241,5 +241,9 @@ function getUserName(string $user_id = ""): string {
 		$user_id = getCallerBaseDir();
 	}
 
+	if (substr($user_id, 0, 1) === "?") {
+		return substr($user_id, 1);
+	}
+
 	return $user_id;
 }
